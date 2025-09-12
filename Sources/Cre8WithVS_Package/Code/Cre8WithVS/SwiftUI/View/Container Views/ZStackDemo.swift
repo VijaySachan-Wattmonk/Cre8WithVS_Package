caa7173c182b11681290a25ee5cfed61453706fc
Vijay Sachan <vijaysachan@wattmonk.com>
2025-09-12 17:39:41 +0530

    --

--- Sources/Cre8WithVS_Package/Code/Cre8WithVS/SwiftUI/View/Container Views/ZStackDemo.swift
+++ Sources/Cre8WithVS_Package/Code/Cre8WithVS/SwiftUI/View/Container Views/ZStackDemo.swift
@@ -19,6 +19,10 @@ enum StackAlignment: String, CaseIterable, Hashable {
     case trailing
     case leadingFirstTextBaseline
     case trailingFirstTextBaseline
+    case centerFirstTextBaseline
+    case leadingLastTextBaseline
+    case centerLastTextBaseline
+    case trailingLastTextBaseline
     var alignment: Alignment{
         switch self {
         case .topLeading: return .topLeading
@@ -32,30 +36,38 @@ enum StackAlignment: String, CaseIterable, Hashable {
         case .trailing: return .trailing
         case .leadingFirstTextBaseline: return .leadingFirstTextBaseline
         case .trailingFirstTextBaseline: return .trailingFirstTextBaseline
+        case .centerFirstTextBaseline: return .centerFirstTextBaseline
+        case .leadingLastTextBaseline: return .leadingLastTextBaseline
+        case .centerLastTextBaseline: return .centerLastTextBaseline
+        case .trailingLastTextBaseline: return .trailingLastTextBaseline
         }
     }
     
     var title: String {
         switch self {
-        case .topLeading: return "Top Leading"
-        case .topTrailing: return "Top Trailing"
-        case .center: return "Center"
-        case .bottomLeading: return "Bottom Leading"
-        case .bottomTrailing: return "Bottom Trailing"
-        case .top: return "Top"
-        case .bottom: return "Bottom"
-        case .leading: return "Leading"
-        case .trailing: return "Trailing"
-        case .leadingFirstTextBaseline: return "Leading First Text Baseline"
-        case .trailingFirstTextBaseline: return "Trailing First Text Baseline"
+        case .topLeading: return ".topLeading"
+        case .topTrailing: return ".topTrailing"
+        case .center: return ".center"
+        case .bottomLeading: return ".bottomLeading"
+        case .bottomTrailing: return ".bottomTrailing"
+        case .top: return ".top"
+        case .bottom: return ".bottom"
+        case .leading: return ".leading"
+        case .trailing: return ".trailing"
+        case .leadingFirstTextBaseline: return ".leadingFirstTextBaseline"
+        case .trailingFirstTextBaseline: return ".trailingFirstTextBaseline"
+        case .centerFirstTextBaseline: return ".centerFirstTextBaseline"
+        case .leadingLastTextBaseline: return ".leadingLastTextBaseline"
+        case .centerLastTextBaseline: return ".centerLastTextBaseline"
+        case .trailingLastTextBaseline: return ".trailingLastTextBaseline"
         }
     }
 }
-
-struct ZStackDemo: View {
+struct ZStackDemo: View{
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 24) {
+//                BaselineGuidesDemoView()
                 Text("ZStack Alignment Examples")
                     .font(.headline)
                     .frame(maxWidth: .infinity)
@@ -120,19 +132,7 @@ struct ZStackDemo: View {
                 }
                 Divider()
                 
-                Text("Getting text baseline guides").font(.title2).bold()
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("10. Alignment: \(StackAlignment.leadingFirstTextBaseline.title)")
-                    AlignmentDemoView(stackAlignment: .leadingFirstTextBaseline)
-                }
-                Divider()
-                
-                VStack(alignment: .leading, spacing: 8) {
-                    Text("11. Alignment: \(StackAlignment.trailingFirstTextBaseline.title)")
-                    AlignmentDemoView(stackAlignment: .trailingFirstTextBaseline)
-                }
-                Divider()
+                BaselineGuidesDemoView()
             }
             .padding()
         }
@@ -155,6 +155,69 @@ struct AlignmentDemoView: View{
                 .foregroundColor(.white)
                 .cornerRadius(6)
             
+        }
+    }
+}
+
+struct CommonBaselineAlignmentView: View {
+    let alignment: Alignment
+    var body: some View {
+        ZStack(alignment: alignment) {
+//            RoundedRectangle(cornerRadius: 10)
+//            HStack(alignment:.lastTextBaseline){
+                Text("0. Text at 0 index some more text, some more text, some more text Text at 0 index")
+                .font(.largeTitle)//.background(Color.blue)
+            Text("1. Text at 1 index some more text, some more text, some more text some more text, some more text, some more text some more text, some more text, some more text Text at 1 index")
+                .font(.caption)
+//            }
+            Divider().frame(height: 1).background(Color.red)
+        }.frame(height: 100).background(Color.gray.opacity(0.15))
+            
+    }
+}
+
+struct BaselineGuidesDemoView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 24) {
+            Text("Getting text baseline guides").font(.title2).bold()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("10. Alignment: \(StackAlignment.leadingFirstTextBaseline.title)")
+                CommonBaselineAlignmentView(
+                    alignment: .leadingFirstTextBaseline
+                )
+            }
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("11. Alignment: \(StackAlignment.trailingFirstTextBaseline.title)")
+                CommonBaselineAlignmentView(alignment: .trailingFirstTextBaseline)
+            }
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("12. Alignment: \(StackAlignment.centerFirstTextBaseline.title)")
+                CommonBaselineAlignmentView(alignment: .centerFirstTextBaseline)
+            }
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("13. Alignment: \(StackAlignment.leadingLastTextBaseline.title)")
+                CommonBaselineAlignmentView(alignment: .leadingLastTextBaseline)
+            }
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("14. Alignment: \(StackAlignment.centerLastTextBaseline.title)")
+                CommonBaselineAlignmentView(alignment: .centerLastTextBaseline)
+            }
+            Divider()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("15. Alignment: \(StackAlignment.trailingLastTextBaseline.title)")
+                CommonBaselineAlignmentView(alignment: .trailingLastTextBaseline)
+            }
+            Divider()
         }
     }
 }
